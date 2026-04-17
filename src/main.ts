3f75d56ad3a17e7c57c1e5ae72487cc5032d6ea9
Vance McGrady <vancemcgrady@gmail.com>
2026-04-17 13:33:07 -0700

    coordinates

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ const background   = new Background(sceneManager.scene);
 const terrain      = new Terrain(sceneManager.scene);
 const character    = new Character();
 const input        = new InputManager();
+const coordsEl     = document.getElementById('coords')!;
 
 sceneManager.scene.add(character.object);
 
@@ -30,6 +31,8 @@ function loop() {
 
   sceneManager.followTarget(pos);
   sceneManager.render();
+
+  coordsEl.textContent = `x  ${pos.x.toFixed(1)}\ny  ${pos.y.toFixed(1)}\nz  ${pos.z.toFixed(1)}`;
 }
 
 loop();
